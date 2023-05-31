b772322c47a597ff5a2869ca1f037f9a7df3edb0
ericlombo <ericlombo1999@gmail.com>
2023-05-31 12:52:41 +0300

    first commit

---
+++ rick.js
@@ -0,0 +1,24 @@
+const section = document.querySelector('.characters')
+const all = document.querySelector('.all')
+async function fetchData() {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json()
+    console.log(data)
+    data.results.forEach(function (character) {
+        const container = document.createElement('div')
+        container.classList.add('char-card')
+        container.innerHTML = `
+            <div class="cardimg"><img src="${character.image}"/></div>
+            <h2>${character.name}</h2>
+            <h3>${character.gender}</h3>
+            ${character.origin.name}
+            ${character.location.name}
+            ${character.status}
+            ${character.species}
+        `
+
+        all.appendChild(container)
+    })
+}
+
+fetchData()
